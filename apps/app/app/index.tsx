6240478f170464b380b901a82cc4e018fbd169e2
jhyunwoo <jhyunwoo0228@gmail.com>
2024-10-07 15:37:24 +0900

    delete unnecessary files

---
+++ apps/app/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Home() {
+  return (
+    <View className={"w-full h-screen flex items-center justify-center"}>
+      <Text>Hello</Text>
+    </View>
+  );
+}
